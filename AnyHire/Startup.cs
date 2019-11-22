f7ff0f047134357f559a27d959969f4c7090f671
deba1 <dsarker333@gmail.com>
2019-11-23 00:39:17 +0600

    Startup

---
+++ AnyHire/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnyHire.Startup))]
+namespace AnyHire
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
