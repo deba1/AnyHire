3787cb3c7be8aab04e5529c76a9b3ab2291ffce3
Habibur013 <58086894+Habibur013@users.noreply.github.com>
2019-11-27 15:47:09 +0600

    Repo

--- AnyHire/Models/AnyHire.Designer.cs
+++ AnyHire/Models/AnyHire.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Projects\C#\AnyHire\AnyHire\Models\AnyHire.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\HP\Documents\GitHub\AnyHire\AnyHire\Models\AnyHire.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
